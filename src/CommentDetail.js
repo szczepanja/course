b956e75aa62ec4262a382992b574f7ee3d15d73a
Anna Szczepanik <szczepanikanja@gmail.com>
2021-10-17 19:30:31 +0200

    update all props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,11 +1,10 @@
 import React from "react";
-import faker from "faker";
 
 const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.image} />
       </a>
       <div className="content">
         <a href="/" className="author">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
@@ -9,21 +10,25 @@ const App = () => {
         author="Sam"
         date="Today at 6:00PM"
         content="Woah what a nice face 👩‍❤️‍👩👵🏽👩‍❤️‍👩👩‍❤️‍👩👩‍❤️‍👩"
+        image={faker.image.avatar()}
       />
       <CommentDetail
         author="Alex"
         date="Monday at 2:00PM"
         content="You look amazing!"
+        image={faker.image.avatar()}
       />
       <CommentDetail
         author="Jane"
         date="Yesterday at 1:00AM"
         content="Hey girl heeeey! 🦝"
+        image={faker.image.avatar()}
       />
       <CommentDetail
         author="Lola"
         date="Sunday at 9:00AM"
         content="Bjutiful! 🎉"
+        image={faker.image.avatar()}
       />
     </div>
   );
