20acb0634845cbbd10c9c622657a563226849d3c
Anna Szczepanik <szczepanikanja@gmail.com>
2021-10-17 18:20:05 +0200

    update environment for next project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  const button = { text: "click!" };
-  const label = "enter name:";
-
-  return (
-    <div>
-      <label className="label" htmlFor="name">
-        {label}
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: "blue", color: "white" }}>
-        {button.text}
-      </button>
-    </div>
-  );
-};
+const App = () => {};
 
 ReactDOM.render(
   <React.StrictMode>
