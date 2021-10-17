30fe40ace58bee19ce2c8232d9a4135702c342c7
Anna Szczepanik <szczepanikanja@gmail.com>
2021-10-17 19:50:47 +0200

    add comment details to approval card

--- src/index.js
+++ src/index.js
@@ -7,31 +7,31 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        date="Today at 6:00PM"
-        content="Woah what a nice face 👩‍❤️‍👩👩‍❤️‍👩👩‍❤️‍👩"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        date="Monday at 2:00PM"
-        content="You look amazing!"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        date="Yesterday at 1:00AM"
-        content="Hey girl heeeey! 🦝"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Lola"
-        date="Sunday at 9:00AM"
-        content="Bjutiful! 🎉"
-        image={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          date="Today at 6:00PM"
+          content="Woah what a nice face 👩‍❤️‍👩👩‍❤️‍👩👩‍❤️‍👩"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          date="Monday at 2:00PM"
+          content="You look amazing!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          date="Yesterday at 1:00AM"
+          content="Hey girl heeeey! 🦝"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
