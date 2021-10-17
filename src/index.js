61cada5cbc038d691a648aae915f7c7db56ea2cd
Anna Szczepanik <szczepanikanja@gmail.com>
2021-10-17 15:29:49 +0200

    add function for button clicking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText() {
+  return "click on me!";
+}
+
 const App = () => {
-  return <div>hejka</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(
